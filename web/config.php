30c844d50d2c1133ea83e80b00804a4e88e4e141
You2php <34645738+You2php@users.noreply.github.com>
2018-01-25 17:34:10 +0800

    Create config.php

---
+++ web/config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', '*********************************');//这里改成您的api key
+define('GJ_CODE', 'KR');//国家代码
+define('SITE_NAME', 'Youtube');//网站名字
+define('TITLENAME', 'Youtube');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', '123@123.com');//邮箱
+?>
