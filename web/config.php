a1580fcb133b627d7920bcc2dc097f97193def1b
onplus <31188782+onplus@users.noreply.github.com>
2018-01-25 21:22:42 +0800

    getenv
    admin:123456

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', getenv("APIKEY"));
+define('GJ_CODE', getenv("GJ_CODE"));
+define('SITE_NAME',getenv("SITE_NAME"));
+define('TITLENAME', getenv("TITLENAME"));
+define('EN2DEKEY',getenv("EN2DEKEY"));
+define('EMAIL', getenv("EMAIL"));
 ?>
