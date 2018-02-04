fcbe1d161dac3ec63de84441d39c17f567c5a53c
Dawingwan <wingwanwin@gmail.com>
2018-02-05 10:54:16 +1300

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
+define('APIKEY', 'AIzaSyCFTsCYd7WNDXqkqAhKX4MUdCGcgFxKE3w');//这里改成您的api key
 define('GJ_CODE', 'KR');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
