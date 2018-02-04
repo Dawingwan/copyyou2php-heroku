79db40ea0d9a39ce6b8b2d6f876e2fe0647622ec
Dawingwan <wingwanwin@gmail.com>
2018-02-05 10:55:25 +1300

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCFTsCYd7WNDXqkqAhKX4MUdCGcgFxKE3w');//这里改成您的api key
 define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
+define('SITE_NAME', '老万油管');//网站名字
+define('TITLENAME', '油管万');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', 'vprintnz@gmail.com');//邮箱
 ?>
