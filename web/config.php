26259b1c7feab4df660431d6fcd2eefd4044344c
Dawingwan <wingwanwin@gmail.com>
2018-02-05 10:27:58 +1300

    我自己的you2php-heroku

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
+define('APIKEY','AIzaSyCFTsCYd7WNDXqkqAhKX4MUdCGcgFxKE3w');//改
 define('GJ_CODE', getenv("GJ_CODE"));
 define('SITE_NAME',getenv("SITE_NAME"));
 define('TITLENAME', getenv("TITLENAME"));
