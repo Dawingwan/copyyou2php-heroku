e156d462efd826519777d8cf0acdd8d934b21c26
Dawingwan <wingwanwin@gmail.com>
2018-02-06 14:12:02 +1300

    Update footer.php

--- web/footer.php
+++ web/footer.php
@@ -3,7 +3,7 @@
             Copyright © 2015-<?php echo date('Y');?> All Rights Reserved </br>
         </p>
         <p  class="text-white  m-1 p-1 h6">
-            Powered by <a href="https://you2php.github.io/" target="_blank" class="text-white font-italic">YOU2PHP</a> 
+            Powered by <a href="https://github.com/You2php/you2php" target="_blank" class="text-white font-italic">Github共享程序</a> 
             <a href="./embed/upgrade.php" target="_blank" class="text-white ml-2">检查更新</a>
         </p>
     </footer>
